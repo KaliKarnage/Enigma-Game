47cf0ca11df32aa5bc2b5c018a8cc35cf33b648d
KaliKarnage <ayukesan1@gmail.com>
2024-03-13 23:19:20 -0600

    Added Audio Snapshots

---
+++ Assets/Scripts/ChangeSnapshot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ChangeSnapshot : MonoBehaviour
+{
+    public AudioMixerSnapshot newSnapshot; // The new snapshot to transition to
+    public float transitionTime = 1.0f; // The time it should take to transition
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // When the trigger happens, transition to the new snapshot
+        newSnapshot.TransitionTo(transitionTime);
+    }
+}
